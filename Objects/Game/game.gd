78f58b41def5758edaf185cd00b7da65234d619e
Jesse <jessew9602@gmail.com>
2024-06-12 23:08:44 +0900

    No errors in game_step_event() in editor

--- Objects/Game/game.gd
+++ Objects/Game/game.gd
@@ -114,270 +114,247 @@ func game_step_event():
 	#/**
 	 #* The game's step event.
 	 #*/
-	if (true): 
-		game.players[0] = noone     #players (used when "with( )" structures will not work)
-		game.players_length = 0
-		with character
+	if (true):
+		self.players[0] = null     #players (used when "with( )" structures will not work)
+		self.players_length = 0
+		var all_characters = gml.get_all_instances("character")
+		for character_instance in all_characters:
 
 		  #necessary to reset the "viscid" movement from a moving solid
-		  viscid_movement_ok=1
-		  #store the characters in the game.players variable
-		  game.players[game.players_length] = id
-		  game.players_length+=1
+			character_instance.viscid_movement_ok=1
+		  #store the characters in the self.players variable
+			self.players[self.players_length] = character_instance#.id
+			self.players_length+=1
 
 
 		#since we are not using GM's hspeed and vspeed variables, we need to add in decimal support ourselves (so 0.25 will only move 1 pixel every 4 steps, for example)
-		game.time+=1
+		self.time+=1
 		#we don't want the time to grow too large
-		if game.time>100000000:
-		  game.time=0
+		if self.time>100000000:
+			self.time=0
 
 		#moves all of the solids so that none of them collide with the character
-		with moving_solid
+		var all_moving_solids = gml.get_all_instances("moving_solid")
+		for moving_solid_instance in all_moving_solids:
 
-		  #applies the acceleration
-		  x_vel+=x_acc
-		  y_vel+=y_acc
-		  #approximates the "active" variables
-		  if approximately_zero(x_vel):
-			x_vel=0
-		  if approximately_zero(y_vel):
-			y_vel=0
-		  if approximately_zero(x_acc):
-			x_acc=0
-		  if approximately_zero(y_acc):
-			y_acc=0
-		  #moves the solid, pushes the character, carries the character, and stops if the character will be crushed by another solid:
-		  mst_x_prev=position.x
-		  mst_y_prev=position.y
-		  #change the decimal arguments to integer variables with relation to time
-		  x_velFrac=frac(abs(x_vel))
-		  y_velFrac=frac(abs(y_vel))
-		  x_velInteger=0
-		  y_velInteger=0
-		  if x_velFrac!=0:
-			if round(1/x_velFrac):!=0
-			   x_velInteger=(game.time % round(1/x_velFrac)=0)
-		  if y_velFrac!=0:
-			if round(1/y_velFrac):!=0
-			  y_velInteger=(game.time % round(1/y_velFrac)=0)
-		  x_velInteger+=floor(abs(x_vel))
-		  y_velInteger+=floor(abs(y_vel))
-		  if x_vel<0:
-			x_velInteger*=-1
-		  if y_vel<0:
-			y_velInteger*=-1
-		  x_velInteger=round(x_velInteger)
-		  y_velInteger=round(y_velInteger)
-		  #calculate the collision bounds of the character -- we'll need it later
-		  with character
-			calculate_collision_bounds()
-		  solid_is_near_players = 0    #whether the solid is near either of the players
-		  #determine if the solid is close to a player:
-		  for(i=0i<game.players_lengthi+=1)
+		 	 #applies the acceleration
+			moving_solid_instance.x_vel+=moving_solid_instance.x_acc
+			moving_solid_instance.y_vel+=moving_solid_instance.y_acc
+		  	#approximates the "active" variables
+			if Collision.approximately_zero(moving_solid_instance.x_vel):
+				moving_solid_instance.x_vel=0
+			if Collision.approximately_zero(moving_solid_instance.y_vel):
+				moving_solid_instance.y_vel=0
+			if Collision.approximately_zero(moving_solid_instance.x_acc):
+				moving_solid_instance.x_acc=0
+			if Collision.approximately_zero(moving_solid_instance.y_acc):
+				moving_solid_instance.y_acc=0
+	  		#moves the solid, pushes the character, carries the character, and stops if the character will be crushed by another solid:
+			moving_solid_instance.mst_x_prev=moving_solid_instance.position.x
+			moving_solid_instance.mst_y_prev=moving_solid_instance.position.y
+	 		 #change the decimal arguments to integer variables with relation to time
+			moving_solid_instance.x_vel_frac=gml.frac(abs(moving_solid_instance.x_vel))
+			moving_solid_instance.y_vel_frac=gml.frac(abs(moving_solid_instance.y_vel))
+			moving_solid_instance.x_vel_integer=0
+			moving_solid_instance.y_vel_integer=0
+			if moving_solid_instance.x_vel_frac!=0:
+				if round(1/moving_solid_instance.x_vel_frac)!=0:
+					if self.time % round(1/moving_solid_instance.x_vel_frac)==0:
+						moving_solid_instance.x_vel_integer = 1
+					else:
+						moving_solid_instance.x_vel_integer = 0
+			if moving_solid_instance.y_vel_frac!=0:
+				if round(1/moving_solid_instance.y_vel_frac)!=0:
+					if self.time % round(1/moving_solid_instance.y_vel_frac) == 0:
+						moving_solid_instance.y_vel_integer = 1
+					else:
+						moving_solid_instance.y_vel_integer = 0
+			moving_solid_instance.x_vel_integer+=floor(abs(moving_solid_instance.x_vel))
+			moving_solid_instance.y_vel_integer+=floor(abs(moving_solid_instance.y_vel))
+			if moving_solid_instance.x_vel<0:
+				moving_solid_instance.x_vel_integer*=-1
+			if moving_solid_instance.y_vel<0:
+				moving_solid_instance.y_vel_integer*=-1
+			moving_solid_instance.x_vel_integer=round(moving_solid_instance.x_vel_integer)
+			moving_solid_instance.y_vel_integer=round(moving_solid_instance.y_vel_integer)
+	 		 #calculate the collision bounds of the character -- we'll need it later
+			for character_instance in all_characters:
+				character_instance.calculate_collision_bounds()
+			moving_solid_instance.solid_is_near_players = 0    #whether the solid is near either of the players
+	  		#determine if the solid is close to a player:
+			for i in range(self.players_length):
+				if Collision.is_collision_rectangle(moving_solid_instance.position.x-abs(moving_solid_instance.x_vel_integer)-moving_solid_instance.sprite_xoffset-2,moving_solid_instance.position.y-abs(moving_solid_instance.y_vel_integer)-moving_solid_instance.sprite_yoffset-2,moving_solid_instance.position.x+moving_solid_instance.sprite_width+abs(moving_solid_instance.x_vel_integer)-moving_solid_instance.sprite_xoffset+2,moving_solid_instance.position.y+moving_solid_instance.sprite_height+abs(moving_solid_instance.y_vel_integer)-moving_solid_instance.sprite_yoffset+2,self.players[i].lb,self.players[i].tb,self.players[i].rb,self.players[i].bb):
+					moving_solid_instance.solid_is_near_players = 1
+		
+	  
+			if(moving_solid_instance.solid_is_near_players):
+				#solid is moving to the right
+				if moving_solid_instance.x_vel_integer>0:
+					moving_solid_instance.break_now=0    #whether we should break out of the movement loop because the character is stuck
+					for x in range(moving_solid_instance.position.x, moving_solid_instance.mst_x_prev + moving_solid_instance.x_vel_integer, 1):
 		  
-			if is_collision_rectangle(position.x-abs(x_velInteger)-sprite_xoffset-2,position.y-abs(y_velInteger)-sprite_yoffset-2,position.x+sprite_width+abs(x_velInteger)-sprite_xoffset+2,position.y+sprite_height+abs(y_velInteger)-sprite_yoffset+2,game.players[i].lb,game.players[i].tb,game.players[i].rb,game.players[i].bb):
+						for i in range(0, self.players_length):
 			
-			  solid_is_near_players = 1    
+							if(moving_solid_instance.viscid_top and Collision.is_collision_character_top(1,self.players[i]) and ((self.players[i]).viscid_movement_ok==1 or (self.players[i]).viscid_movement_ok==2)):
+			  
+								if Collision.is_collision_right(1, self.players[i])==0:
+									self.players[i].position.x+=1
+									self.players[i].viscid_movement_ok=2
+				  
+			  
+							elif Collision.is_collision_character_right(1,self.players[i]):
+								self.players[i].collision=Collision.is_collision_right(1, self.players[i])
+								if self.players[i].collision:
+									moving_solid_instance.break_now = 1
+									break
+				
+								self.players[i].position.x+=1
+			  
+			
+						if moving_solid_instance.break_now:
+							break
+		  
+		
+				#solid is moving to the left
+				if moving_solid_instance.x_vel_integer<0:
+		
+					moving_solid_instance.break_now=0    #whether we should break out of the movement loop because the character is stuck
+					for x in range(moving_solid_instance.position.x, moving_solid_instance.mst_x_prev + moving_solid_instance.x_vel_integer, -1):
+		  
+						for i in range(0, self.players_length):
+			
+							if moving_solid_instance.viscid_top and Collision.is_collision_character_top(1,self.players[i]) and (self.players[i].viscid_movement_ok==1 or self.players[i].viscid_movement_ok==2):
+			  
+								if Collision.is_collision_left(1, self.players[i])==0:
+				  
+									moving_solid_instance.position.x-=1
+									moving_solid_instance.viscid_movement_ok=2
+				  
+			  
+							elif Collision.is_collision_character_left(1,self.players[i]):
+			  
+								self.players[i].collision=Collision.is_collision_left(1, self.players[i])
+								if self.players[i].collision:
+									moving_solid_instance.break_now = 1
+									break
+				
+								self.players[i].position.x-=1
+			  
+			
+							if moving_solid_instance.break_now:
 			 
-		break
-			
+								break
 		  
-		  if(solid_is_near_players)
-		  
-			#solid is moving to the right
-			if x_velInteger>0:
-			
-			  break_now=0    #whether we should break out of the movement loop because the character is stuck
-			  for(position.x=xx<mst_x_prev+x_velIntegerx+=1)
-			  
-				for(i=0i<game.players_lengthi+=1)
+		
+				#solid is moving down
+				if moving_solid_instance.y_vel_integer>0:
 				
-				  if(viscid_top and is_collision_character_top(1,game.players[i]) and ((game.players[i]).viscid_movement_ok=1 or (game.players[i]).viscid_movement_ok=2))
+					moving_solid_instance.break_now=0    #whether we should break out of the movement loop because the character is stuck
+					for y in range(moving_solid_instance.position.y, moving_solid_instance.mst_y_prev + moving_solid_instance.y_vel_integer, 1):
 				  
-					with game.players[i]
-					  if is_collision_right(1):=0
+						for i in range(0, self.players_length):
+					
+							if moving_solid_instance.viscid_top and Collision.is_collision_character_top(2,self.players[i]):
 					  
-						position.x+=1
-						viscid_movement_ok=2
+								#since we do not want to include the solid that is pulling the character down,
+								#we must alter the position of the solid to get around this dilemma
+								position.y+=5
+								if Collision.is_collision_bottom(1, self.players[1])==0:
+									position.y+=1
+								position.y-=5
 					  
-				  
-				  elif is_collision_character_right(1,game.players[i]):
-				  
-					with game.players[i]
-					  collision=is_collision_right(1)
-					if game.players[i].collision:
-					
-					  break_now = 1
-					 
-		break
-					
-					game.players[i].position.x+=1
-				  
-				
-				if break_now:
-				 
-		break
-			  
-			
-			#solid is moving to the left
-			if x_velInteger<0:
-			
-			  break_now=0    #whether we should break out of the movement loop because the character is stuck
-			  for(position.x=xx>mst_x_prev+x_velIntegerx-=1)
-			  
-				for(i=0i<game.players_lengthi+=1)
-				
-				  if viscid_top and is_collision_character_top(1,game.players[i]): and (game.players[i].viscid_movement_ok=1 or game.players[i].viscid_movement_ok=2)
-				  
-					with game.players[i]
-					  if is_collision_left(1):=0
+							elif Collision.is_collision_character_bottom(1,self.players[i]):
 					  
-						position.x-=1
-						viscid_movement_ok=2
+								self.players[i].collision=Collision.is_collision_bottom(1, self.players[i])
+								if self.players[i].collision:
+									moving_solid_instance.break_now = 1	 
+									break
+						
+								self.players[i].position.y+=1
 					  
-				  
-				  elif is_collision_character_left(1,game.players[i]):
-				  
-					with game.players[i]
-					  collision=is_collision_left(1)
-					if game.players[i].collision:
-					
-					  break_now = 1
-					 
-		break
-					
-					game.players[i].position.x-=1
-				  
-				
-				if break_now:
-				 
-		break
-			  
-			
-			#solid is moving down
-			if y_velInteger>0:
-			
-			  break_now=0    #whether we should
-		break out of the movement loop because the character is stuck
-			  for(position.y=yy<mst_y_prev+y_velIntegery+=1)
-			  
-				for(i=0i<game.players_lengthi+=1)
-				
-				  if viscid_top and is_collision_character_top(2,game.players[i]):
-				  
-					#since we do not want to include the solid that is pulling the character down,
-					#we must alter the position of the solid to get around this dilemma
-					position.y+=5
-					with game.players[i]
-					  if is_collision_bottom(1):=0
-						position.y+=1
-					position.y-=5
-				  
-				  elif is_collision_character_bottom(1,game.players[i]):
-				  
-					with game.players[i]
-					  collision=is_collision_bottom(1)
-					if game.players[i].collision:
-					
-					  break_now = 1
-					 
-		break
-					
-					game.players[i].position.y+=1
-				  
-				
-				if break_now:
-				 
-		break
-			  
-			
-			#solid is moving up
-			if y_velInteger<0:
-			
-			  break_now=0    #whether we should
-		break out of the movement loop because the character is stuck
-			  for(position.y=yy>mst_y_prev+y_velIntegery-=1)
-			  
-				for(i=0i<game.players_lengthi+=1)
-				
-				  #push the character up regardless of the viscid properties of the solid top
-				  if is_collision_character_top(1,game.players[i]):
-				   
-					with game.players[i]
-					  collision=is_collision_top(1)
-					if game.players[i].collision:
-					
-					  break_now = 1
-					 
-		break
-					
-					game.players[i].position.y-=1
-				  
-				  if is_collision_character_bottom(1,game.players[i]):
-				  
-					#variable jumping causes the character to get stuck to the bottom of a moving solid
-					#that is moving faster than 1 pixel per step upwards, so we need this code
-					if game.players[i].jump_time<game.players[i].jump_timeTotal:
-					
-					  game.players[i].y_vel=-2
-					  game.players[i].jump_time=game.players[i].jump_timeTotal
-					
-				  
-				
-				if break_now:
-				 
-		break
-			  
-			
-			with character
-			
-			  if viscid_movement_ok=2:
-				viscid_movement_ok=0
-			
+						if moving_solid_instance.break_now:
+							break
 		  
-		  else:
+		
+				#solid is moving up
+				if moving_solid_instance.y_vel_integer<0:
+				
+					moving_solid_instance.break_now=0    #whether we should break out of the movement loop because the character is stuck
+					for y in range(moving_solid_instance.position.y, moving_solid_instance.mst_y_prev + moving_solid_instance.y_vel_integer, -1):
+				  
+						for i in range(0, self.players_length):
+					
+					  		#push the character up regardless of the viscid properties of the solid top
+							if Collision.is_collision_character_top(1,self.players[i]):
+
+								self.players[i].collision=Collision.is_collision_top(1, self.players[i])
+								if self.players[i].collision:
+									moving_solid_instance.break_now = 1
+									break
+						
+								self.players[i].position.y-=1
+					  
+							if Collision.is_collision_character_bottom(1,self.players[i]):
+					  
+								#variable jumping causes the character to get stuck to the bottom of a moving solid
+								#that is moving faster than 1 pixel per step upwards, so we need this code
+								if self.players[i].jump_time<self.players[i].jump_time_total:
+						
+									self.players[i].y_vel=-2
+									self.players[i].jump_time=self.players[i].jump_time_total
+						
+						if moving_solid_instance.break_now:
+							break
 		  
-			position.x+=x_velInteger
-			position.y+=y_velInteger
-		  
-		  
-		  #/*
-			#if (place_meeting(position.x,position.y+1,item)):
-			#
-				#y_vel = 0
-				#my_grav = 0
-				##
-		#break
-			#
-			#else:
-			#
-				#my_grav = 1
-			#
-		  #*/
+		
+				for character_instance in all_characters:
+		
+					if character_instance.viscid_movement_ok==2:
+						character_instance.viscid_movement_ok=0
+		
+	  
+			else:
+	  
+				moving_solid_instance.position.x+=moving_solid_instance.x_vel_integer
+				moving_solid_instance.position.y+=moving_solid_instance.y_vel_integer
+	  
+	  
+	  #/*
+		#if (place_meeting(position.x,position.y+1,item)):
+		#
+			#y_vel = 0
+			#my_grav = 0
+			##
+	#break
+		#
+		#else:
+		#
+			#my_grav = 1
+		#
+	  #*/
 
 		#finished moving_solid code
 		#accelerates the moveable_solid objects downwards
-		with moveable_solid
+		var all_moveable_solids = gml.get_all_instances("moveable_solid")
+		for moveable_solid_instance in all_moveable_solids:
 
-			if ((position.x > view_xview[0]-16 and position.x < view_xview[0] + view_wview[0] and:
-				position.y > view_yview[0]-16 and position.y < view_yview[0] + view_hview[0]))
+			if ((moveable_solid_instance.position.x > gml.view('xview')-16 and moveable_solid_instance.position.x < gml.view('xview') + gml.view('wview')[0] and
+				moveable_solid_instance.position.y > gml.view('yview')-16 and moveable_solid_instance.position.y < gml.view('yview')[0] + gml.view('hview'))):
 			
-				y_m_prev = position.y
-				y_vel += my_grav
-				if (y_vel > 8): y_vel = 8
-				#y_vel+=game.moveable_solid_grav
+				moveable_solid_instance.y_m_prev = moveable_solid_instance.position.y
+				moveable_solid_instance.y_vel += moveable_solid_instance.my_grav
+				if (moveable_solid_instance.y_vel > 8): moveable_solid_instance.y_vel = 8
+				#y_vel+=self.moveable_solid_grav
 				#moves the moveable solid down
-				for (position.y = position.y position.y < y_m_prev + y_vel position.y += 1)
+				for y in range(moveable_solid_instance.position.y, moveable_solid_instance.y_m_prev + moveable_solid_instance.y_vel, 1):
 				
 				#if there is a collision with a solid or the character one pixel below the moveable solid, we want it to stop:
 				#is there a (precise) collision
-					if (place_meeting(position.x, position.y+1, solid)) # or is_collision_character_bottom(2)):
+					if (gml.place_meeting(moveable_solid_instance.position.x, moveable_solid_instance.position.y+1, 'solid')): # or is_collision_character_bottom(2))
 					
-						if (y_vel > my_grav): Audio.play_sound(global.snd_thud)
-						y_vel = 0
-					   
-		break
+						if (moveable_solid_instance.y_vel > moveable_solid_instance.my_grav): Audio.play_sound(global.snd_thud)
+						moveable_solid_instance.y_vel = 0		   
+						break
 					
 					
 					#scr_moveable_solid_recurse_drop()
