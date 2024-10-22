f32dda597cf974fb0c4dd51d4800da223d42f3ac
Jesse <jessew9602@gmail.com>
2024-10-22 23:17:34 +0900

    Added some objects with no associated code

---
+++ Objects/basis/menu/menu.gd
@@ -0,0 +1,16 @@
+extends GMObject
+class_name Menu
+
+func _ready():
+	object_setup()
+
+
+func _physics_process(delta):
+	object_tick()
+
+
+func _process(delta):
+	object_process(delta)
+
+
+#--- Object functions
--- Parent Classes/Menu.gd
+++
@@ -1,9 +0,0 @@
-class_name Menu
-
-var sprite
-var solid = false
-var visible = true
-var depth = 0
-var persistent = false
-var parent
-var mask
