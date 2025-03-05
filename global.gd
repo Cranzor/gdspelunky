d8e67649effbfdd1466a4755940d245133e41939
Jesse <jessew9602@gmail.com>
2025-03-05 14:29:42 +0900

    Fixed error with enemy_kills not being able to increment

--- global.gd
+++ global.gd
@@ -70,7 +70,9 @@ var emeralds
 var end_room_x
 var end_room_y
 var enemy_deaths
-var enemy_kills = {}
+var enemy_kills: Dictionary[int, int] = {0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, \
+				6 : 0, 7 : 0, 8 : 0, 9 : 0, 10 : 0, 11 : 0, 12 : 0, 13 : 0, 14 : 0, 15 : 0, \
+				16 : 0, 17 : 0, 18 : 0, 19 : 0, 20 : 0, 21 : 0}
 var entity_gen
 var exit_x
 var exit_y
