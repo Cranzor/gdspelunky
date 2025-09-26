aad9dace81639e9514182102c567d5cad0b9e9b9
Jesse <jessew9602@gmail.com>
2025-09-26 18:43:05 +0900

    Added debug highlighting of objects based on the collision cell the mouse occupies

--- collision/debug/draw_grid.gd
+++ collision/debug/draw_grid.gd
@@ -1,7 +1,29 @@
 extends Node2D
 
-@export var cell_size: int = 32
+@onready var cell_size: int = custom_collision.cell_size
 @export var debug_draw_grid: bool = true
+var custom_collision = preload("res://collision/custom_collision.gd").new()
+var debug_highlighting = preload("res://collision/debug/broad_phase_highlighting.gd").new()
+var prior_cell: Vector2
+var prior_objects: Array[GMObject]
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		hightlight_objects_based_on_mouse_cell()
+
+
+func hightlight_objects_based_on_mouse_cell():
+	var mouse_cell: Vector2 = custom_collision.get_hash(get_global_mouse_position())
+	if mouse_cell != prior_cell:
+		for object: GMObject in prior_objects:
+			debug_highlighting.reset_color(object)
+		prior_objects.clear()
+		var objects = custom_collision.find_objects_in_grid_cells([mouse_cell])
+		for object: GMObject in objects:
+			debug_highlighting.highlight_color(object, Color("f50aff"))
+			prior_objects.append(object)
+
 
 func _draw() -> void:
 	if debug_draw_grid:
