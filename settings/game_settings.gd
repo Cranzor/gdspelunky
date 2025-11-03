d09e54e3a915b8d3e9c98ffca5a6218fabae8e31
Jesse <jessew9602@gmail.com>
2025-11-03 12:33:11 +0900

    Began work on new pause menu

--- settings/game_settings.gd
+++ settings/game_settings.gd
@@ -6,4 +6,7 @@ static var rumble: bool = true
 static var screen_shake: bool = true
 static var display_fps: bool = false
 static var auto_run: bool = false
+static var sound_effects_volume
+static var music_volume
+static var high_detail: bool = true
 static var resolution
