7d65dcc32109b01b120d772838d4109cdd65266e
Jesse <jessew9602@gmail.com>
2025-11-17 12:32:25 +0900

    Set drawn rectangle size to fill the screen if in widescreen, and set title fade-in text to be centered

--- components/drawing/draw_rectangle/draw_rectangle.gd
+++ components/drawing/draw_rectangle/draw_rectangle.gd
@@ -4,6 +4,7 @@ extends ColorRect
 func draw_rectangle(x1, y1, x2, y2, outline) -> void:
 	position = Vector2(x1, y1)
 	size = Vector2(x2 - x1, y2 - y1)
+	size.x += View.offset
 	color = gml.draw_color
 	color.a = gml.draw_alpha
 	show()
