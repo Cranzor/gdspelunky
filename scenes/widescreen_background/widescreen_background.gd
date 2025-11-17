16e852d4ef255234725ef01b0e66ac021c41ef6b
Jesse <jessew9602@gmail.com>
2025-11-17 22:37:18 +0900

    Made WidescreenBackground node have two sprites and implemented two different block types for transitions between different areas

--- scenes/widescreen_background/widescreen_background.gd
+++ scenes/widescreen_background/widescreen_background.gd
@@ -1,33 +1,52 @@
-extends Sprite2D
+extends Node2D
 
+@onready var background1: Sprite2D = $Background1
+@onready var background2: Sprite2D = $Background2
 @export var brick_textures: Array[Texture2D]
 @export var lush_textures: Array[Texture2D]
 @export var dark_textures: Array[Texture2D]
 @export var temple_textures: Array[Texture2D]
 
 @onready var room_to_textures: Dictionary = {
-	"title" : brick_textures,
-	"transition1" : brick_textures,
-	"transition1x" : brick_textures,
-	"transition2" : lush_textures,
+	"transition1x" : [brick_textures, lush_textures],
+	"transition2" : [lush_textures],
+	"transition2x" : [lush_textures, dark_textures],
+	"transition3" : [dark_textures],
+	"transition3x" : [dark_textures, temple_textures],
+	"transition4" : [temple_textures],
+	"other" : [brick_textures],
 }
 
 var texture_size_in_blocks: Vector2 = Vector2(10, 8)
 var block_size: Vector2 = Vector2(16, 16)
 var default_region_size = Vector2(320, 240)
 
+
 func _ready() -> void:
 	var room_name: String = gml.room_get_name()
-	if room_name in room_to_textures:
-		generate_texture(room_name)
+	set_up_background(room_name)
+
+
+func set_up_background(room_name: StringName) -> void:
+	var level_textures: Array
+	if room_name not in room_to_textures: level_textures = room_to_textures["other"]
+	else: level_textures = room_to_textures[room_name]
+	var composite_texture: Texture2D = generate_texture(level_textures[0])
+	background1.texture = composite_texture
+	
+	if level_textures.size() == 1:
 		set_size(2)
+	elif level_textures.size() > 1:
+		composite_texture = generate_texture(level_textures[1])
+		background2.texture = composite_texture
+		set_size(2, true)
 
 
-func generate_texture(room_name: StringName) -> void:
+func generate_texture(textures_to_use: Array[Texture2D]):
 	var texture_size_in_pixels: Vector2 = texture_size_in_blocks * block_size
 	var composite_image: Image = Image.create_empty(texture_size_in_pixels.x, texture_size_in_pixels.y, false, Image.FORMAT_RGBA8)
-	var textures_to_use: Array[Texture2D] = room_to_textures[room_name]
-	
+
+
 	var x: int = 0
 	var y: int = 0
 	
@@ -35,9 +54,16 @@ func generate_texture(room_name: StringName) -> void:
 		var block_coords: Vector2 = Vector2(x, y)
 		var block_image: Image = textures_to_use[0].get_image()
 		
+		if textures_to_use == brick_textures:
+			var n = randi_range(1,10)
+			if (n == 1): block_image = textures_to_use[3].get_image()
+		
 		var n: int = randi_range(1, 100)
 		if n < 20: block_image = textures_to_use[1].get_image()
 		elif n < 30: block_image = textures_to_use[2].get_image()
+		
+		if textures_to_use == temple_textures and global.city_of_gold:
+			block_image = textures_to_use[3].get_image()
 		
 		composite_image.blit_rect(block_image, Rect2i(Vector2.ZERO, block_size), Vector2(x, y) * block_size)
 		
@@ -47,11 +73,18 @@ func generate_texture(room_name: StringName) -> void:
 			y += 1
 	
 	var composite_texture: Texture2D = ImageTexture.create_from_image(composite_image)
-	texture = composite_texture
-	
+	return composite_texture
 
-func set_size(multipier: int):
+
+func set_size(multipier: int, both_displayed: bool = false):
 	var pixels: int = multipier * default_region_size.x
-	offset.x = -pixels
-	var screens: int = multipier * 2 + 1
-	region_rect = Rect2(0, 0, default_region_size.x * screens, 240)
+	background1.offset.x = -pixels
+	if not both_displayed:
+		var screens: int = multipier * 2 + 1
+		background1.region_rect = Rect2(0, 0, default_region_size.x * screens, 240)
+	else:
+		var background1_screens: int = multipier + 1
+		background1.region_rect = Rect2(0, 0, default_region_size.x * background1_screens, 240)
+		var background2_screens: int = multipier
+		background2.offset.x = default_region_size.x
+		background2.region_rect = Rect2(0, 0, default_region_size.x * background2_screens, 240)
