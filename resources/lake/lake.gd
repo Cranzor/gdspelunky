56b80dd4490f20fd131337254c50deba5eb32904
Jesse <jessew9602@gmail.com>
2025-12-06 00:19:17 +0900

    Added generation of water bottom sprites at lake bottom

--- resources/lake/lake.gd
+++ resources/lake/lake.gd
@@ -3,6 +3,10 @@ extends GMObject
 var size: Vector2i = Vector2i(640, 208)
 
 
+func _ready() -> void:
+	create_water_bottom_sprites()
+
+
 func create_piranhas() -> void:
 	var initial_position: Vector2i = Vector2i(16, 448)
 	var size_in_blocks: Vector2i = Vector2i(40, 12)
@@ -14,3 +18,23 @@ func create_piranhas() -> void:
 			if (not gml.collision_point(block_pos.x, block_pos.y, "solid", 0, 0)):
 				if (gml.rand(1,30) == 1):
 					gml.instance_create(block_pos.x + 4, block_pos.y + 4, Objects.piranha)
+
+
+func create_water_bottom_sprites() -> void:
+	var bottom_left_water_pos: Vector2i = Vector2i(16, 640)
+	for x in range(1, 41): #--- 40 water blocks horizontally
+		var block_pos: Vector2i = Vector2i(x * 16, 640)
+		var lake_bottom: AnimatedSprite2D = preload("res://resources/lake/lake_bottom.tscn").instantiate()
+		add_child(lake_bottom)
+		lake_bottom.global_position = block_pos
+		if gml.rand(1,4) == 1: #--- water_bottom setting adapted from level_generation script
+			lake_bottom.global_position.y -= 16
+			lake_bottom.play("water_bottom_tall")
+		else:
+				var n = gml.rand(1,4)
+				match (n):
+				
+					1: lake_bottom.animation = "water_bottom"
+					2: lake_bottom.animation = "water_bottom2"
+					3: lake_bottom.animation = "water_bottom3"
+					4: lake_bottom.animation = "water_bottom4"
