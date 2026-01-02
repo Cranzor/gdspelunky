f22fc4c7e422b9fed94153730116237e9fafab74
Jesse <jessew9602@gmail.com>
2026-01-02 12:30:25 +0900

    Added Game Modes pause menu option for mods to utilize (hidden by default)

--- scenes/ui/new_menu/menu_screen.gd
+++ scenes/ui/new_menu/menu_screen.gd
@@ -5,4 +5,4 @@ var previous_menu_screen: VBoxContainer
 
 func display_menu_screen() -> void:
 	show()
-	get_node(focus_to_grab).grab_focus()
+	if focus_to_grab: get_node(focus_to_grab).grab_focus()
